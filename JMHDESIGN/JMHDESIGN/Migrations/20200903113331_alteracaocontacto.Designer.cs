ff084ce5a752d96d67e70afc6483e54671e05829
jessicadasilvahenriques <62100786+jessicadasilvahenriques@users.noreply.github.com>
2020-09-05 10:12:48 +0100

    Lixo

--- JMHDESIGN/JMHDESIGN/Migrations/20200903113331_alteracaocontacto.Designer.cs
+++ JMHDESIGN/JMHDESIGN/Migrations/20200903113331_alteracaocontacto.Designer.cs
@@ -4,14 +4,16 @@ using JMHDESIGN.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JMHDESIGN.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200903113331_alteracaocontacto")]
+    partial class alteracaocontacto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
